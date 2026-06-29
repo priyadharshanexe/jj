84fa8780cd604a83c4fcf11fc2c7eeaaabd65eac
priyadharshanexe <priyadharshan.exe@gmail.com>
2026-06-29 16:47:08 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,5 @@
 import unittest
-from app import application
-
+from application import application
 
 class FlaskAppTest(unittest.TestCase):
 
@@ -8,11 +7,10 @@ class FlaskAppTest(unittest.TestCase):
         application.testing = True
         self.client = application.test_client()
 
-    def test_home_page_status(self):
-        """Check whether homepage returns HTTP 200"""
+    def test_home_page(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-
+        
     def test_home_page_content(self):
         """Check if page contains expected heading"""
         response = self.client.get("/")
